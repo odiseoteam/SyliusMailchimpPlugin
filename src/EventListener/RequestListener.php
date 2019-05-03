a80548bf0ad4a2584c0398c0b43ad7e47e691613
Damian Damico <damian@odiseo.com.ar>
2019-05-03 11:23:06 -0300

    add campaing id

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -15,10 +15,13 @@ final class RequestListener
         }
 
         $request = $event->getRequest();
+
         $session = $request->getSession();
-        if (!$session->get('campaingId')) {
-            if ($request->get('mc_cid')) {
-                $session->set('campaingId', $request->get('mc_cid'));
+        if ($session !== null) {
+            if (!$session->get('campaingId')) {
+                if ($request->get('mc_cid')) {
+                    $session->set('campaingId', $request->get('mc_cid'));
+                }
             }
         }
     }
